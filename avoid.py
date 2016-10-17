f8df7f3cc89a23cbedc184d60b36abef56757d73
JaeyoungJang <dkfqkzzz@naver.com>
2016-10-17 18:01:46 +0900

    Add files via upload
    적 등장

--- avoid.py
+++ avoid.py
@@ -4,9 +4,6 @@ import random
 # 화면 가로,세로
 pad_w = 900
 pad_h = 600
-#첫번째,두번째화면 피봇좌표
-back1_x = 450
-back2_x = 1350
 
 # Game object class here
 def handle_events():
@@ -44,14 +41,28 @@ def handle_events():
 class Back1:
     def __init__(self):
         self.image = load_image('back.png')
+        self.back1_x = 450
+
+    def update(self):
+        self.back1_x -= 10
+        if self.back1_x == -450:
+            self.back1_x = 1350
+
     def draw(self):
-        self.image.draw(back1_x,300)
+        self.image.draw(self.back1_x,300)
 
 class Back2:
     def __init__(self):
         self.image = load_image('back2.png')
+        self.back2_x = 1350
+
+    def update(self):
+        self.back2_x -= 10
+        if self.back2_x <= -450:
+            self.back2_x = 1350
+
     def draw(self):
-        self.image.draw(back2_x,300)
+        self.image.draw(self.back2_x,300)
 
 class Miko:
     def __init__(self):
@@ -83,6 +94,19 @@ class Miko:
         if self.state == False:
             self.image.draw(self.x,self.y)
 
+class Enemy1:
+    def __init__(self):
+        self.image = load_image('enemy1.png')
+        self.frame = 0
+        self.x = 1000
+        self.y = random.randint(200,550)
+
+    def update(self):
+        self.frame = self.frame + 5
+        self.x -= 5
+
+    def draw(self):
+        self.image.clip_draw((self.frame%3) * 69,0,74,95,self.x,self.y)
 
 # initialization code
 open_canvas(pad_w,pad_h)
@@ -90,6 +114,7 @@ open_canvas(pad_w,pad_h)
 back1 = Back1()
 back2 = Back2()
 miko = Miko()
+enemy1 = Enemy1()
 
 running = True
 # game main loop code
@@ -101,16 +126,12 @@ while running:
 
     back1.draw()
     back2.draw()
+    back1.update()
+    back2.update()
     miko.draw()
     miko.update()
-
-    #배경
-    back1_x -= 10
-    back2_x -= 10
-    if back1_x == -450:
-        back1_x = 1350
-    if back2_x == -450:
-        back2_x = 1350
+    enemy1.draw()
+    enemy1.update()
 
     update_canvas()
 
