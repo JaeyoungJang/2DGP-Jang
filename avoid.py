acd06b71b2a6adf0f0834eef8d67052fbc8aad55
JaeyoungJang <dkfqkzzz@naver.com>
2016-12-02 16:07:21 +0900

    update
    revise the code

--- avoid.py
+++ avoid.py
@@ -260,8 +260,8 @@ def enter():
     miko = Miko()
     enemy1 = Enemy1()
     enemy2 = Enemy2()
-    enemies1 = [Enemy1() for i in range(15)]
-    enemies2 = [Enemy2() for i in range(12)]
+    enemies1 = [Enemy1() for i in range(13)]
+    enemies2 = [Enemy2() for i in range(9)]
     warning_font = load_font('ENCR10B.TTF', 115)
     score_font = load_font('ENCR10B.TTF', 25)
     score_count = 0
